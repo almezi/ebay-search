f675b093fcaa6c21f5fe8884c3d503d673638e3e
Aulia Mei Azizi <auliameiazizi@gmail.com>
2025-10-30 11:44:19 +0700

    Remove Cucumber test setup for login functionality.

--- features/pageobjects/page.js
+++ features/pageobjects/page.js
@@ -9,9 +9,6 @@ module.exports = class Page {
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    open(path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
-    }
     async openEbay() {
         await browser.url('https://www.ebay.com');
     }
